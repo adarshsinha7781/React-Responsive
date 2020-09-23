7bacb26bdf60b93eb35c01e7736cd3149c94550f
Adarsh Sinha <adarshsinha018@gmail.com>
2020-09-24 00:26:35 +0530

    Updates

---
+++ precache-manifest.e26b92b3c9e1626d231eabf8d0a597d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc848079bc3f4b9e8e4c856833255a2",
+    "url": "//React-Responsive/index.html"
+  },
+  {
+    "revision": "f22770650727d4909d02",
+    "url": "//React-Responsive/static/css/main.d35da748.chunk.css"
+  },
+  {
+    "revision": "924820dec00f4e2bff22",
+    "url": "//React-Responsive/static/js/2.3d079a93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//React-Responsive/static/js/2.3d079a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22770650727d4909d02",
+    "url": "//React-Responsive/static/js/main.7a757c5e.chunk.js"
+  },
+  {
+    "revision": "10d7ea7eb7ac83a599ce",
+    "url": "//React-Responsive/static/js/runtime-main.b0ef003a.js"
+  }
+]);
